cc1d2c9ec1e3854c831970a1d4cb83529564af28
e.monakhova <evmonakhova@mail.ru>
2017-05-06 14:05:06 +0300

    Added main activity layout, job item layout, search icons, ect

--- app/src/main/java/ru/hh/headhunterclient/JobSearchActivity.java
+++ app/src/main/java/ru/hh/headhunterclient/JobSearchActivity.java
@@ -2,6 +2,7 @@ package ru.hh.headhunterclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class JobSearchActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class JobSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_search);
+
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mActionBarToolbar);
     }
 }
